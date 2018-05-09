7c71bb361df4f1587961a4e8389b2f57db421be3
3ndrius <andrzejo1991@gmail.com>
2018-05-09 17:07:59 +0200

    Get data from cms

--- src/App.js
+++ src/App.js
@@ -6,13 +6,39 @@ import './css/App.css';
 //component import
 
 class App extends Component {
+
+  state = {
+    projects: []
+  }
+
+  componentDidMount() {
+    let projetUrl = "http://apiwordpress.cba.pl/wp-json/wp/v2/posts";
+    fetch(projetUrl)
+    .then(response => response.json())
+    .then(response => {
+      this.setState({
+        projects:response
+      })
+    })
+  }
   render() {
+
+    let projects =this.state.projects.map((project, index) => {
+      return (
+        <div key={index}> 
+          <h1>{project.title.rendered}</h1>
+        </div>
+      )
+            
+    })
     return (
       <div className="App">
-       <h1> Hello0 </h1>
+       <h1> Hello </h1>
+       {projects}
       </div>
-    );
-  }
-}
+    );//end return
+
+  }//end render
+};//end class
 
 export default App;
